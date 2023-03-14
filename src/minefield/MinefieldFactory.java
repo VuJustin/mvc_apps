4aee6466193399d12b89b3eab838d22fa838329b
deptrajvjppro <minh.bui@sjsu.edu>
2023-03-13 22:31:20 -0700

    add some project prerequisites features

--- src/minefield/MinefieldFactory.java
+++ src/minefield/MinefieldFactory.java
@@ -1,4 +1,38 @@
 package minefield;
 
-public class MinefieldFactory {
+import mvc.*;
+
+public class MinefieldFactory implements AppFactory {
+
+    public Model makeModel() {
+        return new Minefield();
+    }
+
+    public View makeView(Model m) {
+        return new MinefieldView((Minefield) m);
+    }
+
+    public String[] getEditCommands() {
+        return new String[] { "Change" };
+    }
+
+    // source added 3/15 to support text fields
+    public Command makeEditCommand(Model model, String type, Object source) {
+        if (type == "Change")
+            return new MoveCommand(model);
+        return null;
+    }
+
+    public String getTitle() {
+        return "Stop Light Simulator";
+    }
+
+    public String[] getHelp() {
+        return new String[] { "click Change to cycle through colors" };
+    }
+
+    public String about() {
+        return "Stoplight Simulator version 1.0. Copyright 2020 by Cyberdellic Designs";
+    }
+
 }
--- src/minefield/MinefieldPanel.java
+++ src/minefield/MinefieldPanel.java
@@ -1,4 +1,50 @@
 package minefield;
 
-public class MinefieldPanel {
+import javax.swing.JButton;
+
+import mvc.AppFactory;
+import mvc.AppPanel;
+
+public class MinefieldPanel extends AppPanel {
+    private JButton northButton, southButton, eastButton, westButton,
+            northeastButton, northwestButton, southeastButton, southwestButton;
+
+    public MinefieldPanel(AppFactory factory) {
+        super(factory);
+
+        northButton = new JButton("N");
+        northButton.addActionListener(this);
+        southButton = new JButton("S");
+        southButton.addActionListener(this);
+        eastButton = new JButton("E");
+        eastButton.addActionListener(this);
+        westButton = new JButton("W");
+        westButton.addActionListener(this);
+        northeastButton = new JButton("NE");
+        northeastButton.addActionListener(this);
+        northwestButton = new JButton("NW");
+        northwestButton.addActionListener(this);
+        southeastButton = new JButton("SE");
+        southeastButton.addActionListener(this);
+        southwestButton = new JButton("SW");
+        southwestButton.addActionListener(this);
+
+        // Was orginally controlPanel.add, changed to super.add()
+        // Fixed
+        super.insertComponent(northButton);
+        super.insertComponent(southButton);
+        super.insertComponent(eastButton);
+        super.insertComponent(westButton);
+        super.insertComponent(northeastButton);
+        super.insertComponent(northwestButton);
+        super.insertComponent(southeastButton);
+        super.insertComponent(southwestButton);
+
+    }
+
+    public static void main(String[] args) {
+        AppFactory factory = new MinefieldFactory();
+        AppPanel panel = new MinefieldPanel(factory);
+        panel.display();
+    }
 }
