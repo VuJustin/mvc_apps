ddfa5dbcfdd3af5c57f972f5d4796a6a8c2fb38b
Justin Vu <justinhvu@gmail.com>
2023-03-15 17:30:04 -0700

    Added Patch to Minefield, Cell to MinefieldView. Adding some items about Dimension, and tweaked Minefield & Minefield View constructors a little bit

--- src/minefield/Minefield.java
+++ src/minefield/Minefield.java
@@ -4,5 +4,25 @@ import mvc.*;
 
 public class Minefield extends Model {
     public static int mine_percent = 5;
+    private int dimensions;
+    private Patch field[][];
 
+    public Minefield(){
+        // Setting up field dimensions
+        dimensions = 10;
+        // Creating new minefield w/ associated dimensions
+        field = new Patch[dimensions][dimensions];
+        // Initializing all patches
+        for(int i = 0; i < dimensions; i++){
+            for(int j = 0; j < dimensions; j++){
+                field[i][j] = new Patch();
+            }
+        }
+    }
+    public int getDimensions(){ return dimensions; }
+    public void setDimensions(int d){ dimensions = d;}
+
+    public Patch getPatch(int row, int col){
+        return field[row][col];
+    }
 }
